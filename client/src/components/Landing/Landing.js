e010f91e29eefc72561530df8919bc75927fc300
Royce Copeland <you@example.com>
2017-10-16 21:07:54 -0400

    adds what I hope will be the journal entry code

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -11,6 +11,7 @@ import SidebarDay from "./SidebarDay";
 //import Clock from "./Clock";
 //import backgroundAPI from "../../utils/backgroundAPI";
 import {Timeline, TimelineEvent, TimelineBlip} from "./Timeline";
+import { Input, TextArea, FormBtn } from "./Entries";
 
 import "./Landing.css";
 
@@ -79,20 +80,18 @@ class Landing extends Component {
             <div>
                 <SidebarDay events={this.state.events}/>
                 <Nav />
-                {/* <Timeline/>
-                <TimelineEvent/>
-                <TimelineBlip/> */}
+            
                 <Timeline>
                     <TimelineEvent title="John Doe sent a SMS"     /*title=props.title*/
                                 createdAt="2016-09-12 10:06 PM"
-                                icon={<i className="material-icons md-18">textsms</i>}
+                             //   icon={<i className="material-icons md-18">textsms</i>}
                     >
                         I received the payment for $543. Should be shipping the item within a couple of hours.
                     </TimelineEvent>
                     <TimelineEvent
                         title="You sent an email to John Doe"
                         createdAt="2016-09-11 09:06 AM"
-                        icon={<i className="material-icons md-18">email</i>}
+                     //   icon={<i className="material-icons md-18">email</i>}
                     >
                         Like we talked, you said that you would share the shipment details? This is an urgent order and so I
                             am losing patience. Can you expedite the process and pls do share the details asap. Consider this a
