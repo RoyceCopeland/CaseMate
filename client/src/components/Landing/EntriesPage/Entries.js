a3b50227d3db4b658978c2e3d420e4d7484eba5d
Royce Copeland <you@example.com>
2017-10-21 13:16:45 -0400

    removes unneeded files and folders

--- client/src/components/Landing/EntriesPage/Entries.js
+++ client/src/components/Landing/EntriesPage/Entries.js
@@ -1,17 +1,14 @@
-import React from "react";
-import Books from "./pages/Books";
-
-const Entries = () => (
-  <div className="container-fluid">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-      
-          <Books />
-        </div>
-      </div>
-    </div>
-  </div>
-);
-
-export default Entries;
+ import React from "react";
+ import Books from "./pages/Books"
+ const Entries = () => (
+   <div className="container-fluid">
+     <div className="container">
+       <div className="row">
+         <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">    
+           <Books />
+         </div>
+       </div>
+     </div>
+   </div>
+ )
+ export default Entries;
