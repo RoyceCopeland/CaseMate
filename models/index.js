53f64fed4166254840b989f23da8de02abb54093
Joshua Dodd <joshuadodd@gmail.com>
2017-10-24 21:14:52 -0400

    pushs dots

--- models/index.js
+++ models/index.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose');
-
 module.exports.connect = (uri) => {
   Entries: require("./Entry")
   mongoose.connect(
-    
-  	uri,
-  	{ 
-  		useMongoClient: true
-  	}
+
+    uri,
+    {
+      useMongoClient: true
+    }
   );
 
-  db.once("open", function() {
-    console.log("mongoose connection successful"); 
-    });
-    
+
   // plug in the promise library:
   mongoose.Promise = global.Promise;
 
@@ -24,13 +20,6 @@ module.exports.connect = (uri) => {
 
   // load models
   require('./User');
-<<<<<<< HEAD
-  // require('./Skill');
-  // require('./Subtask');
-  // require('./ToDo');
-  // require('./Streak');
-=======
-   require('./Entry');
+  require('./entry');
 
->>>>>>> f85dd615044bf8b3abeb378ad6d9743e74ef079f
 };
