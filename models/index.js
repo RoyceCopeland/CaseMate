97a110614b4d86d0430ecb5896d6b851ebe38599
Royce Copeland <you@example.com>
2017-10-24 21:18:26 -0400

    temp commit

--- models/index.js
+++ models/index.js
@@ -1,29 +1,3 @@
-const mongoose = require('mongoose');
-
-module.exports.connect = (uri) => {
-  Entries: require("./Entry")
-  mongoose.connect(
-    
-  	uri,
-  	{ 
-  		useMongoClient: true
-  	}
-  );
-
-  db.once("open", function() {
-    console.log("mongoose connection successful"); 
-    });
-    
-  // plug in the promise library:
-  mongoose.Promise = global.Promise;
-
-  mongoose.connection.on('error', (err) => {
-    console.error(`Mongoose connection error: ${err}`);
-    process.exit(1);
-  });
-
-  // load models
-  require('./User');
-   require('./Entry');
-
+module.exports = {
+  entry: require("./entry")
 };
