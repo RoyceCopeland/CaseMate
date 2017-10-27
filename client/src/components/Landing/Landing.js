b54d82d22d2feb33e9cc8be03bcb87aea7d2b65d
Royce Copeland <you@example.com>
2017-10-26 21:32:08 -0400

    adds db functionality

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -17,7 +17,7 @@ import { MessageBox,
   Dropdown,
   SideBar,
   Popup,
-  PhotoMessage } from "./Chatbox";
+  PhotoMessage } from "./ChatBox";
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
