b8012b9a825096580a58ca39566aca52e26b656c
Joshua Dodd <joshuadodd@gmail.com>
2017-10-29 16:26:25 -0400

    pulls updates

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ## Overview
 
 CaseMate will provide an effective client portal system that follows agile development methodology. The intent is to provide a service that helps clients store case-related data.
+
+
+
+![GitHub Logo](../img/justice_bkgnd.jpg)
+Format: ![Alt Text](url)
--- client/src/components/Landing/Nav/Nav.js
+++ client/src/components/Landing/Nav/Nav.js
@@ -1,35 +1,27 @@
 import React from "react";
 //import App from "./EntriesPage/App";
 
-const Nav = props => 
-<div className="navbar-fixed">
-<nav className="cyan darken-1 z-depth-0">
-  <div className="nav-wrapper">
-    <div className="row">
-      <div className="col s12">
-        <a href="#" className="brand-logo" target="_blank">CaseMate Client Portal</a>
-        <ul className="right hide-on-med-and-down">
-          <li onClick={props.click}><a href="#">New Journal Entry</a></li>
-          <li><a href="#" target="_blank">Chat</a></li>
-          <li><a href="#" target="_blank">FAQ</a></li>
-          <li><a href="#" target="_blank">Log Out</a></li>
-        </ul>
+const Nav = props =>
+  <div className="navbar-fixed">
+    <nav className="cyan darken-1 z-depth-0">
+      <div className="nav-wrapper">
+        <div className="row">
+          <div className="col s12">
+            <a href="#" className="brand-logo" target="_blank">CaseMate Client Portal</a>
+            <ul className="right hide-on-med-and-down">
+              <li onClick={props.click}><a href="#">New Journal Entry</a></li>
+              <li><a href="#" target="_blank">Chat</a></li>
+              <li><a href="#" target="_blank">FAQ</a></li>
+              <li onClick={props.click}><a href="#">Log Out</a></li>
+
+            </ul>
+          </div>
+
+        </div>
       </div>
-   
-    </div>
-  
-  
-    {/* <ul className="side-nav" id="mobile-demo">
-      <li><a href="https://codepen.io/collection/nbBqgY/" target="_blank">Sass</a></li>
-      <li><a href="https://codepen.io/collection/nbBqgY/" target="_blank">Components</a></li>
-      <li><a href="https://codepen.io/collection/nbBqgY/" target="_blank">Javascript</a></li>
-      <li><a href="https://codepen.io/collection/nbBqgY/" target="_blank">Mobile</a></li>
-    </ul> */}
- 
- 
+    </nav>
   </div>
-</nav>
-</div>
-;
+  ;
+
 
 export default Nav;
