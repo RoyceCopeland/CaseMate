13d1fe7d8b6bc626e86cfee2dedd13c1704cd2a5
Royce Copeland <roycecopeland@Royces-iMac.local>
2017-10-30 17:27:54 -0400

    adds chatbox component work

---
+++ client/src/components/Landing/ChatExample/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+
+const render = Component => {
+	ReactDOM.render(
+		<Component/>,
+		document.getElementById('app'),
+	);
+};
+
+render(App);
+
+
+
+export { default } from "./App";
--- client/src/components/Landing/Chatbox/index.js
+++ client/src/components/Landing/Chatbox/index.js
@@ -31,4 +31,3 @@ export {
     
 };
 
-//export { default } from "./ChatList";
