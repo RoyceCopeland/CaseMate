a057b8bee958fe27138a18549794cf4a3669e6f1
Royce Copeland <you@example.com>
2017-11-01 15:08:40 -0400

    adds button color consistency

--- client/src/components/Landing/EntriesPage/components/Form/FormBtn.js
+++ client/src/components/Landing/EntriesPage/components/Form/FormBtn.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const FormBtn = props =>
-  <button {...props} style={{ float: "right" }} className="btn btn-success">
+  <button {...props} style={{ float: "right", margin: "10px", background: "rgb(69, 179, 199" }} className="btn btn-success">
     {props.children}
   </button>;
