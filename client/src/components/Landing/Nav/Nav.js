298042fc6bce912115a45b40ca7c8aebf30c2a67
Royce Copeland <you@example.com>
2017-11-02 17:37:09 -0400

    working checkpoint

--- client/src/components/Landing/Nav/Nav.js
+++ client/src/components/Landing/Nav/Nav.js
@@ -1,32 +1,32 @@
 import React from "react";
 //import App from "./EntriesPage/App";
 import Auth from "../../../utils/Auth";
-import "./Nav.css"
+import "./Nav.css";
 
-
-const Nav = props => 
-<div className="navbar-fixed">
-<nav className="#424242 grey darken-3 z-depth-0">
-  <div className="nav-wrapper">
-    <div className="row">
-      <div className="col s12">
-        <a href="#" className="brand-logo"></a>
-        <ul className="right hide-on-med-and-down">
-          <li onClick={props.click}><a href="#">New Journal Entry</a></li>
-          <li onClick={props.showChatHandler}><a>Chat</a></li>
-          <li onClick={props.deAuth}><a href="#">Log Out</a></li></ul>
+const Nav = props => (
+  <div className="navbar-fixed">
+    <nav className="#424242 grey darken-3 z-depth-0">
+      <div className="nav-wrapper">
+        <div className="row">
+          <div className="col s12">
+            <a href="#" className="brand-logo" />
+            <ul className="right hide-on-med-and-down">
+              <li onClick={props.click}>
+                <a href="#">New Journal Entry</a>
+              </li>
+              <li onClick={props.showChatHandler}>
+                <a>Chat</a>
+              </li>
+              <li onClick={props.deAuth}>
+                <a href="#">Log Out</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        }
       </div>
-   
-    </div>
- }
- 
+    </nav>
   </div>
-
-</nav>
-</div>
-
-
-  ;
-
+);
 
 export default Nav;
